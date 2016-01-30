d5a0dce50faeb59548103c1bdbaa74d4769d8d1b
dethe <dethe@livingcode.org>
2016-01-30 11:10:15 -0800

    dragging and snapping

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
+var GRIDSIZE = 25;
 var PI = Math.PI, cos = Math.cos, sin = Math.sin, rad=Snap.rad;
 
 Snap.plugin(function (Snap, Element, Paper, global, Fragment) {
+
+    function onDrag(dx, dy, x, y, evt){
+        this.moveTo(x, y);
+    }
+
+    function onDragStart(x, y, evt){
+    }
+
+    function onDragEnd(evt){
+        // snap to grid
+        var x = this.num('cx'), y = this.num('cy');
+        var dX = x % GRIDSIZE;
+        if (dX < GRIDSIZE / 2){
+            x -= dX;
+        }else{
+            x += GRIDSIZE - dX;
+        }
+        var dY = y % GRIDSIZE;
+        if (dY < GRIDSIZE / 2){
+            y -= dY;
+        }else{
+            y += GRIDSIZE - dY;
+        }
+        this.moveTo(x,y);
+    }
 
     var pathFns = {
 
@@ -36,6 +62,26 @@ Snap.plugin(function (Snap, Element, Paper, global, Fragment) {
             }
             path.push('Z');
             return path.join(' ');
+        },
+        spiral: function(e){
+            var cx=e.num('cx'), cy=e.num('cy'), r=e.num('r'), num=e.num('num'), len=e.num('len'), rot=e.rad('rot');
+            var theta = PI * 2 / num;
+            var dR = r / (num * len);
+            var path = ['M'];
+            for (var i = 0; i < (num * len); i++){
+                path.push(cos(theta * i + rot) * (dR * i) + cx, sin(theta * i + rot) * (dR * i) + cy);
+            }
+            return path.join(' ');
+        },
+        coffin: function(cx, cy, r){
+        },
+        crescent: function(cx, cy, r){
+        },
+        trigram: function(cx, cy, r, h){
+        },
+        leaf: function(cx, cy, r){
+        },
+        heart: function(cx, cy, r){
         }
     }
 
@@ -44,22 +90,32 @@ Snap.plugin(function (Snap, Element, Paper, global, Fragment) {
     Paper.prototype.asterisk = function(cx, cy, r, num){
         return this.path()
             .attr('r', r).attr('num', num).attr('type', 'asterisk')
-            .moveTo(cx, cy);
+            .moveTo(cx, cy).setup();
     };
 
     Paper.prototype.rpolygon = function(cx, cy, r, num){
         return this.path()
             .attr('r', r).attr('num', num).attr('type', 'polygon')
-            .moveTo(cx, cy);
+            .moveTo(cx, cy).setup();
     };
 
     Paper.prototype.star = function(cx, cy, r, num){
         return this.path()
             .attr('r', r).attr('num', num).attr('type', 'star')
-            .moveTo(cx, cy);
+            .moveTo(cx, cy).setup();
+    };
+
+    Paper.prototype.spiral = function(cx, cy, r, num, len){
+        return this.path()
+            .attr('r', r).attr('num', num).attr('len', len).attr('type', 'spiral')
+            .moveTo(cx, cy).setup();
     };
 
     // Element extensions
+
+    Element.prototype.setup = function(){
+        return this.drag(onDrag, onDragStart, onDragEnd);
+    };
 
     Element.prototype.num = function(name){
         return Number(this.attr(name));
@@ -91,10 +147,11 @@ Snap.plugin(function (Snap, Element, Paper, global, Fragment) {
 });
 
 var ATTRS = {
-    fill: '#FFF',
+    fill: 'transparent',
     stroke: '#000',
     strokeWidth: 5
 }
+
 var s = Snap(WIDTH, HEIGHT);
 
 var circle = s.rpolygon(50,50,25, 20).attr(ATTRS);
@@ -110,3 +167,6 @@ var poly6 = s.rpolygon(125, 275, 25, 6).attr(ATTRS);
 var star5 = s.star(125, 350, 25, 5).attr(ATTRS);
 
 var star7 = s.star(200, 50, 25, 7).attr(ATTRS);
+var spiral1 = s.spiral(200, 125, 25, 500, 3).attr(ATTRS);
+var spiral2 = s.spiral(200, 200, 25, 20, .75).attr(ATTRS);
+
